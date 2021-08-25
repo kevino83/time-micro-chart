1b7e8bef89825ce221af0d921a7b7df4c40a6b11
Alessandro Boschini <alessandro.boschini@rcslab.it>
2021-08-25 19:43:41 +0200

    github pages for time-micro-chart

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import DateMicroChart from "./date/DateMicroChart";
-import DaysMicroChart from "./days/DaysMicroChart";
-import HoursMicroChart from "./hours/HoursMicroChart";
-
-export {
-    DaysMicroChart,
-    DateMicroChart,
-    HoursMicroChart
-}
